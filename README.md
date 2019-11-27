cfd8d424eb4d409a12ef35829f0760c6749fb146
zhangji02 <zhangji02@zuoyebang.com>
2019-11-27 15:16:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+My Personal Webpack
+===================
+
+This is a repository for practicing webpack config.
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './common/reset.less';
+import Vue from 'vue';
+import App from './App.vue';
+
+let vm = new Vue({
+  el: '#app',
+  render: h => h(App)
+});
